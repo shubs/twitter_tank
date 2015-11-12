d661cfb607f234a1a9a091c28edb695395351b3a
SHARMA Shubham <shubham@sharma.fr>
2015-11-12 17:44:35 -0500

    queues and worker set

---
+++ workers/examinitor.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import pika
+import time
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+
+channel = connection.channel()
+channel.queue_declare(queue='examine_q', durable=True)
+print ' [*] Waiting for messages. To exit press CTRL+C'
+
+def callback(ch, method, properties, body):
+    print " [x] Received %r" % (body,)
+    time.sleep(1)
+    print " [x] Done"
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback,
+                      queue='examine_q')
+
+channel.start_consuming()
