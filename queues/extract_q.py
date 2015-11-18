4b34a56f8943b7a216a0fa7e67e151afeb31ccad
SHARMA Shubham <shubham@sharma.fr>
2015-11-18 15:25:39 -0500

    queue for examine ok

--- queues/extract_q.py
+++ queues/extract_q.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
 import pika
 import sys
+import tweepy
+from ConfigParser import SafeConfigParser
+
+parser = SafeConfigParser()
+parser.read('credentials.cfg')
+
+consumer_key = parser.get('Twitter3', 'consumer_key')
+consumer_secret = parser.get('Twitter3', 'consumer_secret')
+access_token = parser.get('Twitter3', 'access_token')
+access_token_secret = parser.get('Twitter3', 'access_token_secret')
+
+print consumer_key
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+
+user = api.get_user('gsempe')
+
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
@@ -8,12 +29,13 @@ channel = connection.channel()
 
 channel.queue_declare(queue='extract_q', durable=True)
 
-message = ' '.join(sys.argv[1:]) or "Hello World!"
-channel.basic_publish(exchange='',
-                      routing_key='extract_q',
-                      body=message,
-                      properties=pika.BasicProperties(
-                         delivery_mode = 2, # make message persistent
-                      ))
-print " [x] Sent %r" % (message,)
+for friend in user.followers_ids():
+	message = str(friend)
+	channel.basic_publish(exchange='',
+	                      routing_key='extract_q',
+	                      body=message,
+	                      properties=pika.BasicProperties(
+	                         delivery_mode = 2, # make message persistent
+	                      ))
+	print " [x] Sent %r" % (message,)
 connection.close()
--- workers/examinitor.py
+++ workers/examinitor.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 import pika
 import time
+from ConfigParser import SafeConfigParser
+
+parser = SafeConfigParser()
+parser.read('credentials.cfg')
+
+consumer_key = parser.get('Twitter3', 'consumer_key')
+consumer_secret = parser.get('Twitter3', 'consumer_secret')
+access_token = parser.get('Twitter3', 'access_token')
+access_token_secret = parser.get('Twitter3', 'access_token_secret')
+
+print consumer_key
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
@@ -12,6 +28,10 @@ print ' [*] Waiting for messages. To exit press CTRL+C'
 def callback(ch, method, properties, body):
     print " [x] Received %r" % (body,)
     time.sleep(1)
+    #lookup information
+    #add to db..
+    #add to DB
+
     print " [x] Done"
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
